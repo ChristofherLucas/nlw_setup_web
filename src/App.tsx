ddcf8602ac43847c2c66fc0c2a09c9aa0fc7b7c6
ChristofherLucas <lucaschristofher05@gmail.com>
2023-01-20 23:46:42 -0300

    chore: initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Habit } from "./components/Habits";
+
+export function App() {
+  return (
+    <div>
+      <Habit />
+    </div>
+  );
+}
---
+++ src/components/Habits.tsx
@@ -0,0 +1,7 @@
+export function Habit() {
+  return (
+    <div className="bg-violet-600 w-6 rounded m-2 text-center justify-center">
+      1
+    </div>
+  );
+}
